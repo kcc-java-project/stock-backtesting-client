7179023b7398d6dad6549fc40b4c43216bf7c7d8
YunSung-Choi97 <sunsu6765@gmail.com>
2025-06-22 18:29:20 +0900

    chore: eslint 기본 규칙 설정 및 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,77 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import jsEslint from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import importOrder from "eslint-plugin-import";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tsEslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default tsEslint.config(
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [jsEslint.configs.recommended, tsEslint.configs.recommended],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "import": importOrder,
+      "@stylistic": stylistic,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
       ],
-    },
-  },
-)
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/semi": "error",
+      "@stylistic/quotes": [
+        "error",
+        "double",
+        {
+          "avoidEscape": true,
+          "allowTemplateLiterals": true
+        }
+      ],
+      "@stylistic/jsx-quotes": [
+        "error",
+        "prefer-double"
+      ],
+      "@stylistic/comma-dangle": ["error", "never"],
+      "@stylistic/member-delimiter-style": [
+        "warn",
+        {
+          "singleline": {
+            "delimiter": "semi",
+            "requireLast": true
+          }
+        }
+      ],
+      "@stylistic/no-multiple-empty-lines": [
+        "error",
+        {
+          "max": 1
+        }
+      ],
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            ["builtin","external"],
+            ["internal","index","parent","sibling"],
+            ["type","unknown"]
+          ],
+          "pathGroups": [],
+          "pathGroupsExcludedImportTypes": [],
+          "alphabetize": {"order": "asc"}
+        }
+      ]
+    }
+  }
+);
